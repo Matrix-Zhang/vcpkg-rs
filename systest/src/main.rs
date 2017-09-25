6153d0ef753788a091ba5aa2493ca5585f73bc03
Jim McGrath <jimmc2@gmail.com>
2017-09-25 11:42:58 -0500

    rustfmt

--- systest/src/main.rs
+++ systest/src/main.rs
@@ -7,17 +7,26 @@ use std::ffi::CStr;
 
 fn main() {
     println!("curl version is {:?}!", curl::Version::get().version());
-    
-    unsafe{  println!("zlib version is {:?}!", CStr::from_ptr(libz_sys::zlibVersion()));}
- 
- //unsafe{  println!("openssl version is {:?}!", CStr::from_ptr(openssl_sys::SSLEAY_VERSION));}
-    unsafe{ openssl_sys::SSL_library_init();}
-//  println!("openssl version is {}!", openssl_sys::OPENSSL_VERSION);
 
-   let mut major = 0;
-        let mut minor = 0;
-        let mut patch = 0;
- unsafe{       zmq_sys::zmq_version(&mut major, &mut minor, &mut patch);
-        }       println!("zmq version {}.{}.{}", major, minor, patch);
-//  unsafe {let ctx = zmq_sys::zmq_init(1); }
+    unsafe {
+        println!(
+            "zlib version is {:?}!",
+            CStr::from_ptr(libz_sys::zlibVersion())
+        );
+    }
+
+    //unsafe{  println!("openssl version is {:?}!", CStr::from_ptr(openssl_sys::SSLEAY_VERSION));}
+    unsafe {
+        openssl_sys::SSL_library_init();
+    }
+    //  println!("openssl version is {}!", openssl_sys::OPENSSL_VERSION);
+
+    let mut major = 0;
+    let mut minor = 0;
+    let mut patch = 0;
+    unsafe {
+        zmq_sys::zmq_version(&mut major, &mut minor, &mut patch);
+    }
+    println!("zmq version {}.{}.{}", major, minor, patch);
+    //  unsafe {let ctx = zmq_sys::zmq_init(1); }
 }
